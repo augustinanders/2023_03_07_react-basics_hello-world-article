7dbd8d0b50fbcf4c6df89b4b334bd6ba6757294b
Augustin Anders <121796762+augustinanders@users.noreply.github.com>
2023-03-07 14:18:44 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import React from "react";
+import HelloWorldArticle from "./components/HelloWorldArticle.js";
+import "./styles.css";
+
+export default function App() {
+  return <HelloWorldArticle />;
+}
---
+++ src/components/HelloWorldArticle.js
@@ -0,0 +1,16 @@
+export default function HelloWorldArticle() {
+  return (
+    <article>
+      <h1>This is a nice heading</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt
+      </p>
+      <p>
+        ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+        exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        Duis
+      </p>
+    </article>
+  );
+}
